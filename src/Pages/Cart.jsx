47cbfb7ea7541b1ec2e1f1131a4ebab1f7045e87
Abdul Hanan Abbasi <146124897+123superhanan@users.noreply.github.com>
2024-09-06 18:14:37 +0500

    cart functionality

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useContext } from 'react'
+import { shopContext } from '../Context/ShopContext'
+import Title from '../Components/Title';
 
 const Cart = () => {
+
+  const {products, currency, cartItem} = useContext(shopContext);
+  const [cartData, setCartData] = useState([]);
+
+  useEffect (() => {
+
+  const tempData = [];
+    for(const items in cartItem){
+      for(const item in cartItem[items]){
+        if(cartItem[items][item] > 0){
+            tempData.push(
+              {
+                _id : items,
+                sizes : item,
+                quantity : cartItem[items][item]
+              }
+             )
+        }
+      }
+    }
+  setCartData(tempData);
+},[cartItem])
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className='border-t pt-14'>
+          <div className='text-2xl mb-3'>
+              <Title text1={"YOUR"} text2={'CART'}/>
+          </div>
+
+          <div>
+            
+          </div>
+
+      </div>
+
+    </>
   )
 }
 
